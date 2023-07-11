ca4575e338602a94db3ba5509be50badbd3effa5
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2023-07-11 20:08:55 +0530

    Connected with mongoDB

--- Project1.py
+++ Project1.py
@@ -1,9 +1,14 @@
 import googleapiclient.discovery
 import googleapiclient.errors
+from pymongo import MongoClient
 
 api_name = "youtube"
 api_version = "v3"
 youtube_api_key = input("Enter API Key:")
 
 youtube = googleapiclient.discovery.build(api_name,api_version,developerKey=youtube_api_key)
+
+myclient = pymongo.MongoClient("mongodb://localhost/27017")
+mydatabase = myclient["youtube"]
+mycollection = mydatabase ["details"]
 

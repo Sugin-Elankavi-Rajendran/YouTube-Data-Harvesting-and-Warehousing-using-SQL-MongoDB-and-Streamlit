377a202b3a9f00610ab58053bf687bce49a4aa61
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2023-07-17 15:19:32 +0530

    Hid the api key

--- Project1.py
+++ Project1.py
@@ -2,12 +2,12 @@ import googleapiclient.discovery
 import pymongo
 from pymongo import MongoClient
 import mysql
+from config import API_KEY
 
 api_service_name = "youtube"
 api_version = "v3"
-api_key = input("Enter API Key: ")
 
-youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=api_key)
+youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=API_KEY)
 
 myclient = MongoClient("mongodb://localhost:27017/")
 mydatabase = myclient["youtube"]

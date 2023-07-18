e5e743a51f6003e2048f0e3a3584837af53cbf73
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2023-07-18 12:02:05 +0530

    viewed channel details

--- Project1.py
+++ Project1.py
@@ -17,6 +17,21 @@ mycollection = mydatabase["details"]
 
 def main():
     st.title("YouTube Channel Migration")
+    
+    channel = st.text_input("Enter Youtube Channel ID: ")
+    
+    if channel:
+        response = youtube.channels().list(part='snippet,statistics',id=channel).execute()
+        channel_name = response['items'][0]
+        channel_title = channel_name['snippet']['title']
+        channel_subscribers = channel_name['statistics']['subscriberCount']
+        channel_videos = channel_name['statistics']['videoCount']
+        channel_description = channel_name ['snippet']['description']
+        
+        st.write (f'Channel Title: {channel_title}')
+        st.write (f'Channel Subscribers: {channel_subscribers}')
+        st.write (f'Channel Videos: {channel_videos}')
+        st.write (f'Channel Description: {channel_description}')
 
 if __name__ == "__main__":
     main()

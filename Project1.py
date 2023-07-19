81ab47ff347cfdf9733a88acfa1d531f7d3bae0a
Sugin Elankavi Rajendran <suginelankavir@gmail.com>
2023-07-19 13:57:10 +0530

    modified the code to the coding standards

--- Project1.py
+++ Project1.py
@@ -15,147 +15,84 @@ mydatabase = myclient["youtube"]
 mycollection = mydatabase["details"]
 
 
+def get_channel_data(channel_id):
+    response = youtube.channels().list(part='snippet,statistics,ContentDetails', id=channel_id).execute()
+    channel_data = response['items'][0]
+
+    channel_title = channel_data['snippet']['title']
+    channel_subscribers = int(channel_data['statistics']['subscriberCount'])
+    channel_views = int(channel_data['statistics']['viewCount'])
+    channel_description = channel_data['snippet']['description']
+    channel_playlist_id = channel_data['contentDetails']['relatedPlaylists']['uploads']
+
+    return {
+        "Channel_Name": {
+            "Channel_Name": channel_title,
+            "Channel_Id": channel_id,
+            "Subscription_Count": channel_subscribers,
+            "Channel_Views": channel_views,
+            "Channel_Description": channel_description,
+            "Playlist_Id": channel_playlist_id
+        }
+    }
+
+
+def get_playlist_data(playlist_id):
+    playlist_response = youtube.playlists().list(part='snippet', playlistId=playlist_id, maxResults=5).execute()
+    playlists = playlist_response['items']
+
+    # while 'nextPageToken' in playlist_response:
+    #     next_page_token = playlist_response['nextPageToken']
+    #     playlist_response = youtube.playlists().list(part='snippet', playlistId=playlist_id, maxResults=5, pageToken=next_page_token).execute()
+    #     playlists.extend(playlist_response['items'])
+
+    return playlists
+
+
+def get_video_data(channel_id):
+    video_response = youtube.search().list(part='snippet', channelId=channel_id, type='video', maxResults=5).execute()
+    videos = video_response['items']
+
+    # while 'nextPageToken' in video_response:
+    #     next_page_token = video_response['nextPageToken']
+    #     video_response = youtube.search().list(part='snippet', channelId=channel_id, type='video', maxResults=5, pageToken=next_page_token).execute()
+    #     videos.extend(video_response['items'])
+
+    return videos
+
+
 def main():
     st.title("YouTube Channel Migration")
-    
-    channel = st.text_input("Enter Youtube Channel ID: ")
-    
-    if channel:
-        response = youtube.channels().list(part='snippet,statistics,ContentDetails',id=channel).execute()
-        channel_data = response['items'][0]
-            
-        channel_id = channel_data['id']
-        channel_title = channel_data['snippet']['title']
-        channel_subscribers = int(channel_data['statistics']['subscriberCount'])
-        channel_views = int(channel_data['statistics']['viewCount'])
-        channel_description = channel_data['snippet']['description']
-        
-        channel_doc = {
-            "Channel_Name": {
-                "Channel_Name": channel_title,
-                "Channel_Id": channel_id,
-                "Subscription_Count": channel_subscribers,
-                "Channel_Views": channel_views,
-                "Channel_Description": channel_description,
-                "Playlist_Id": None
-            }
-        }       
-        
-        st.write(f'Channel Name: {channel_title}')
-        st.write(f'Channel ID: {channel_id}')
-        st.write(f'Channel Subscribers: {channel_subscribers}')
-        st.write(f'Channel Views: {channel_views}')
-        st.write(f'Channel Description: {channel_description}')
-        
-        playlist_response = youtube.playlists().list(part='snippet',channelId=channel,maxResults=5).execute()
-        playlists = playlist_response['items']
 
-        # while 'nextPageToken' in playlist_response:
-        #     next_page_token = playlist_response['nextPageToken']
-        #     playlist_response = youtube.playlists().list(part='snippet', channelId=channel, maxResults=5, pageToken=next_page_token).execute()
-        #     playlists.extend(playlist_response['items'])
+    channel_ids = st.text_area("Enter YouTube Channel IDs (one per line):")
+    channel_ids = channel_ids.strip().split("\n") if channel_ids else []
 
+    for channel_id in channel_ids:
+        channel_data = get_channel_data(channel_id)
+        playlists = get_playlist_data(channel_data["Channel_Name"]["Playlist_Id"])
+        videos = get_video_data(channel_id)
+
+        
+        mycollection.insert_one(channel_data["Channel_Name"])
         for playlist in playlists:
-            playlist_title = playlist['snippet']['title']
-            playlist_id = playlist['id']
-            
-            playlist_doc = {
-                "Playlist_Id": playlist_id,
-                "Playlist_Title": playlist_title,
-                "Videos": {}
-            }
-            
-            channel_doc["Channel_Name"]["Playlist_Id"] = playlist_id
-                
-            st.write(f'Playlist Title: {playlist_title}')
-            st.write(f'Playlist ID: {playlist_id}')
-                    
-            video_response = youtube.playlistItems().list(part='snippet', playlistId=playlist_id, maxResults=50).execute()
-            videos = video_response['items']
-            
-            # while 'nextPageToken' in video_response:
-            #     next_page_token = video_response['nextPageToken']
-            #     video_response = youtube.playlistItems().list(part='snippet', playlistId=playlist_id, maxResults=50,pageToken=next_page_token).execute()
-            #     videos.extend(video_response['items'])
-            
-            for video in videos:
-                video_data = video['snippet']
-                video_id = video_data['resourceId']['videoId']
-                video_title = video_data['title']
-                video_description = video_data['description']
-                tags = video_data.get('tags', [])
-                published_at = video_data['publishedAt']
-                view_count = int(video_data['statistics']['viewCount'])
-                like_count = int(video_data['statistics']['likeCount'])
-                dislike_count = int(video_data['statistics']['dislikeCount'])
-                favorite_count = int(video_data['statistics']['favoriteCount'])
-                comment_count = int(video_data['statistics']['commentCount'])
-                duration = video_data['contentDetails']['duration']
-                thumbnail = video_data['thumbnails']['default']['url']
-                caption_status = video_data['contentDetails'].get('caption', 'Not Available')
-                
-                
-                video_doc = {
-                    "Video_Id": video_id,
-                    "Video_Name": video_title,
-                    "Video_Description": video_description,
-                    "Tags": tags,
-                    "PublishedAt": published_at,
-                    "View_Count": view_count,
-                    "Like_Count": like_count,
-                    "Dislike_Count": dislike_count,
-                    "Favorite_Count": favorite_count,
-                    "Comment_Count": comment_count,
-                    "Duration": duration,
-                    "Thumbnail": thumbnail,
-                    "Caption_Status": caption_status,
-                    "Comments": {}
-                }
-                
-                
-                playlist_doc["Videos"][video_id] = video_doc
-                
-                st.write(f'Video Title: {video_title}')
-                st.write(f'Video ID: {video_id}')
-                
-                comment_response = youtube.commentThreads().list(part='snippet', videoId=video_id,maxResults=20).execute()
-                comments = comment_response['items']
+            mycollection.insert_one({"Playlist": playlist})
+        for video in videos:
+            mycollection.insert_one({"Video": video})
 
-                for comment in comments:
-                    comment_data = comment['snippet']['topLevelComment']['snippet']
-                    comment_id = comment['id']
-                    comment_text = comment_data['textDisplay']
-                    comment_author = comment_data['authorDisplayName']
-                    comment_published_at = comment_data['publishedAt']
-
-                    
-                    comment_doc = {
-                        "Comment_Id": comment_id,
-                        "Comment_Text": comment_text,
-                        "Comment_Author": comment_author,
-                        "Comment_PublishedAt": comment_published_at
-                    }
-
-                    
-                    video_doc["Comments"][comment_id] = comment_doc
-
-                    st.write(f'Comment: {comment_text}')
-                    st.write(f'Author: {comment_author}')
-                    st.write(f'Published At: {comment_published_at}')
-                
-                mycollection.insert_one(video_doc)
-            
-            mycollection.insert_one(playlist_doc)   
         
-        mycollection.insert_one(channel_doc)
-                        
-        migrate_channel = st.checkbox("Migrate this channel")
-        
-        if migrate_channel:
-            st.write(f"Channel {channel_title} is selected for migration!")
-    
-    else:
-        st.write("Invalid channel ID. Please enter a valid YouTube channel ID.")
-        
+        st.write(f"Channel Name: {channel_data['Channel_Name']['Channel_Name']}")
+        st.write(f"Channel ID: {channel_data['Channel_Name']['Channel_Id']}")
+        st.write(f"Channel Subscribers: {channel_data['Channel_Name']['Subscription_Count']}")
+        st.write(f"Channel Views: {channel_data['Channel_Name']['Channel_Views']}")
+        st.write(f"Channel Description: {channel_data['Channel_Name']['Channel_Description']}")
+        for playlist in playlists:
+            st.write(f"Playlist Title: {playlist['snippet']['title']}")
+            st.write(f"Playlist ID: {playlist['id']}")
+        for video in videos:
+            st.write(f"Video Title: {video['snippet']['title']}")
+            st.write(f"Video ID: {video['id']['videoId']}")
+
+        st.write("Data stored in MongoDB!")
+
 if __name__ == "__main__":
     main()
